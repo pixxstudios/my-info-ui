c5ddf2e0bfa097da37320e0b48665056674a13f9
Gagandeep Singh <gagan.ddun@gmail.com>
2020-04-25 21:11:09 +1000

    webpack

---
+++ src/app.js
@@ -0,0 +1,7 @@
+import React from 'react';
+
+const App = () => (
+    <h3>Hello world!!</h3>
+);
+
+export default App;
--- src/main.js
+++ src/main.js
@@ -1,1 +1,6 @@
-console.log('hello');
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+import App from './app';
+
+ReactDOM.render(<App />,document.getElementById('root'));
