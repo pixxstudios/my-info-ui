c4a26015d365ebfd3bd35e8fb1169a3e8fa29f84
Gagandeep Singh <gagan.ddun@gmail.com>
2020-04-25 22:24:11 +1000

    server index.html

--- webpack.config.js
+++ webpack.config.js
@@ -1,10 +1,16 @@
 const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
 
 module.exports = {
   entry: './src/main.js',
   output: {
     path: path.resolve(__dirname, 'dist'),
     filename: 'bundle.js'
+  },
+  devServer: {
+    contentBase: path.join(__dirname, 'public'),
+    compress: true,
+    port: 9000
   },
   module: {
     rules: [
@@ -17,5 +23,11 @@ module.exports = {
       }
     ]
   },
+  plugins: [
+    new HtmlWebpackPlugin({
+      template: './public/index.html',
+      filename: './index.html'
+    })
+  ],
   watch: true
 };
