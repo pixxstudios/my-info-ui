380b4dcac716ebecd68c7cb0147ce7a3d817f365
Gagandeep Singh <gagan.ddun@gmail.com>
2020-05-07 21:16:24 +1000

    added home component

--- src/app.js
+++ src/app.js
@@ -1,7 +1,11 @@
 import React from 'react';
+import Home from './components/home/home';
 
 const App = () => (
-    <h3>Hello world!</h3>
+    <>
+        <h3>Hello world!</h3>
+        <Home />
+    </>
 );
 
 export default App;
