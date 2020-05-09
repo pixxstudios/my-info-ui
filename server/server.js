5fd23cbb34ddf3cf02ac1512eed607ebfff347e4
Gagandeep Singh <gagan.ddun@gmail.com>
2020-05-09 22:36:07 +1000

    db connection

--- server/server.js
+++ server/server.js
@@ -1,10 +1,17 @@
 const express = require('express');
+const db = require('./db.config');
+
+// console.log('db ', db);
 
 const {
     userRoutes
 } = require('./routes/routes');
 
 const app = express();
+
+app.get('/', (req, res) => {
+    res.send('hey');
+});
 
 app.use(userRoutes);
 
