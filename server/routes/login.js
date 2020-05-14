39bfba4fc61581c9aaaf8e03d0042fcfb536ef22
Gagandeep Singh <gagan.ddun@gmail.com>
2020-05-14 22:31:09 +1000

    compare pass

--- server/routes/login.js
+++ server/routes/login.js
@@ -1,9 +1,12 @@
 require('dotenv').config();
 const loginRoute = require('express').Router();
 const jwt = require('jsonwebtoken');
-const { User } = require('../schemas/schemas');
+const bcrypt = require('bcrypt');
+const {
+    User
+} = require('../schemas/schemas');
 
-loginRoute.post('/login', (req, res) => {
+loginRoute.post('/login', (req, res, next) => {
     const email = req.body.email;
     const password = req.body.password;
 
@@ -13,17 +16,39 @@ loginRoute.post('/login', (req, res) => {
         });
     }
 
-    User.findOne({ email })
-    .then(response => {
-        const { _id } = response;
-        const token = jwt.sign({ id: _id }, process.env.TOKEN_SECRET);
-        res.status(200).json({
-            authToken: token
-        });
-    })
-    .catch(err => {
-        res.status(400).send(err);
-    })
+    User.findOne({
+            email
+        })
+        .then(response => {
+            // check if email exists
+            if (!response) {
+                res.status(422).json({
+                    error: 'Invalid email'
+                });
+                return next();
+            }
+
+            // check the password
+            bcrypt.compare(password, response.password)
+                .then(passwordMatch => {
+                    if (passwordMatch) {
+                        // send the jwt token back to client
+                        const {
+                            _id
+                        } = response;
+                        const token = jwt.sign({
+                            id: _id
+                        }, process.env.TOKEN_SECRET);
+                        res.status(200).json({
+                            authToken: token
+                        });
+                    }
+                })
+                .catch(err => console.log('error ', err));
+        })
+        .catch(err => {
+            res.status(400).send(err);
+        })
 });
 
 module.exports = loginRoute;
