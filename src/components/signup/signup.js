69517c23de766657025226372f2f8cfd90d3a2b8
Gagandeep Singh <gagan.ddun@gmail.com>
2020-05-20 22:17:57 +1000

    fix

--- src/components/signup/signup.js
+++ src/components/signup/signup.js
@@ -1,5 +1,32 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
 
-const Signup = () => <h3>Signup</h3>;
+const Signup = () => {
+    const handleOnSubmit = e => {
+        e.preventDefault();
+        
+        console.log('handleOnSubmit');
+    };
+
+    return (<div>
+        <form action="post" onSubmit={handleOnSubmit}>
+            <div>
+                <label htmlFor="email">Email</label>
+                <input type="email" required autoComplete="false" name="email" />
+            </div>
+
+            <div>
+                <label htmlFor="password">Password</label>
+                <input type="password" required autoComplete="false" name="password" />
+            </div>
+
+            <div>
+                <button type="submit">Signup</button>
+            </div>
+        </form>
+
+        <Link to="/">Login</Link>
+    </div>)
+};
 
 export default Signup;
--- webpack.config.js
+++ webpack.config.js
@@ -10,7 +10,8 @@ module.exports = {
   devServer: {
     contentBase: path.join(__dirname, 'public'),
     compress: true,
-    port: 9000
+    port: 9000,
+    historyApiFallback: true
   },
   module: {
     rules: [
