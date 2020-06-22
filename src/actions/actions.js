72049c70629a17b667371e0b5f3bf27a12bae845
Gagandeep Singh <gagan.ddun@gmail.com>
2020-06-22 21:51:23 +1000

    login actions

--- src/actions/actions.js
+++ src/actions/actions.js
@@ -26,3 +26,30 @@ export const onSignupRequest = (dispatch, email, password) => {
     })
     .catch(err => console.log('err', err));        
 };
+
+export const LOGIN_REQUEST = 'LOGIN_REQUEST';
+export const LOGIN_SUCCESS = 'LOGIN_SUCCESS';
+export const LOGIN_FAILURE = 'LOGIN_FAILURE';
+
+export const onLoginRequest = (dispatch, email, password) => {
+    dispatch({
+        type: LOGIN_REQUEST
+    });
+
+    axios.post('http://localhost:9001/register', {email, password})
+    .then(response => {
+        if (response.data.error) {
+            dispatch({
+                type: SIGNUP_FAILURE,
+                message: response.data.error
+            });
+        } else{
+            dispatch({
+                type: SIGNUP_SUCCESS,
+                payload: response,
+                message: response.data.message
+            })
+        }
+    })
+    .catch(err => console.log('err', err));        
+};
--- src/components/login/login.js
+++ src/components/login/login.js
@@ -1,27 +1,42 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { Link } from 'react-router-dom';
+import { useDispatch, useSelector } from 'react-redux';
 import './style.scss';
 
-const Login = () => (
+const Login = () => {
+    const dispatch = useDispatch();
+
+    const [email, setEmail] = useState('');
+    const [password, setPassword] = useState('');
+
+    const { message } = useSelector(state => state);
+
+    const handleOnSubmit = e => {
+        e.preventDefault();
+        onSignupRequest(dispatch, email, password);
+    };
+
+    return (
     <div className="login">
-        <form className="card">
+        <form className="card" onSubmit={handleOnSubmit} >
             <div className="form-group">
                 <label htmlFor="email">Email</label>
-                <input type="email" required autoComplete="false" name="email" />
+                <input type="email" required autoComplete="false" name="email" onChange={e => setEmail(e.target.value)} />
             </div>
 
             <div className="form-group">
                 <label htmlFor="password">Password</label>
-                <input type="password" required autoComplete="false" name="password" />
+                <input type="password" required autoComplete="false" name="password" onChange={e => setPassword(e.target.value)} />
             </div>
 
             <div className="footer">
                 <button className="login-button" type="submit">Login</button>
                 <Link to="/signup" className="button signup">Signup</Link>
             </div>
+            <div className="message">{message}</div>
         </form>
-
     </div>
 );
+}
 
 export default Login;
