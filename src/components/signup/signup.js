3734621f955dd3ae48f1c738f2478d6a350af2c4
Gagandeep Singh <gagan.ddun@gmail.com>
2020-06-22 21:39:32 +1000

    styling updates and fixes

--- src/components/signup/signup.js
+++ src/components/signup/signup.js
@@ -19,26 +19,27 @@ const Signup = () => {
         onSignupRequest(dispatch, email, password);
     };
 
-    return (<div>
-        <form action="post" onSubmit={handleOnSubmit}>
-            <div>
+    return (
+    <div className="signup">
+        <form action="post" onSubmit={handleOnSubmit} className="card">
+            <div className="form-group">
                 <label htmlFor="email">Email</label>
                 <input type="email" required autoComplete="false" name="email"
                     onChange={ e => setEmail(e.target.value) } />
             </div>
 
-            <div>
+            <div className="form-group">
                 <label htmlFor="password">Password</label>
                 <input type="password" required autoComplete="false" name="password"
                     onChange={ e => setPassword(e.target.value) }/>
             </div>
 
-            <div>
-                <button type="submit">Signup</button>
+            <div className="form-group">
+                <button type="submit" className="signup-button">Signup</button>
+                <Link to="/">Login</Link>
             </div>
+            <div className="message">{message}</div>
         </form>
-        <div className="message">{message}</div>
-        <Link to="/">Login</Link>
     </div>)
 };
 
